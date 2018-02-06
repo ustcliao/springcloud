842c2c867e3d05d52aa00e901a41fbc78e160262
ustcliao <ustcliao@qq.com>
2018-02-06 10:08:24 +0800

    add feign service

--- service-feign/src/main/java/org/service/feign/App.java
+++ service-feign/src/main/java/org/service/feign/App.java
@@ -1,13 +1,16 @@
 package org.service.feign;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
--- service-ribbon/src/main/java/org/service/ribbon/HelloService.java
+++ service-ribbon/src/main/java/org/service/ribbon/HelloService.java
@@ -1,8 +1,10 @@
 package org.service.ribbon;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class HelloService {
     @Autowired
     RestTemplate restTemplate;
