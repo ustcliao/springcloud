f25baa8f09b4fec73437042b26c3c9cc9014cb30
ustcliao <ustcliao@qq.com>
2018-02-06 13:59:33 +0800

    add config server

---
+++ config-client/src/main/java/org/config/client/App.java
@@ -0,0 +1,29 @@
+package org.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@RestController
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+    
+
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
--- config-server/src/main/java/org/config/server/App.java
+++ config-server/src/main/java/org/config/server/App.java
@@ -1,13 +1,19 @@
 package org.config.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
+@EnableConfigServer
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
